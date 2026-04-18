a6a8b2c818cc9c1359dbca48467ac76a32b680f1
Martin Lowe <martinlowe673@gmail.com>
2026-04-18 17:09:07 +0100

    state

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SkillCard = () => {
+
+
+type SkillCardProps = {
+ name:string
+}
+const SkillCard = ({name}:SkillCardProps) => {
+    const [liked,setLiked] = useState(false)
+    const likes = liked ? 1 : 0;
+
   return (
     <div>
       SkillCard
+      <h2>{name}</h2>
+      <p>{likes} {likes === 1 ? 'like':'likes'} </p>
+      <button onClick={() => setLiked((current) => !current)} type='button'></button>
     </div>
   )
 }
